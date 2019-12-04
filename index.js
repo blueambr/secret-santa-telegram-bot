ba5a103f0537f8a388d2b850977fa414a5f1bde2
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-12-04 16:12:36 +0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+process.env["NTBA_FIX_319"] = 1;
+
+const csv = require('csvtojson');
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+
+const app = express();
+const csvFilePath = '';
+const port = process.env.PORT || 4000;
+const telegram = new Bot (process.env.TOKEN, { polling: true });
+
+if (csvFilePath) {
+  csv({
+    noheader: true,
+    output: 'csv'
+  })
+  .fromFile(csvFilePath)
+  .then((csvRow) => {
+    console.log(csvRow);
+  });
+};
+
+telegram.on('text', (msg) => {
+  telegram.sendMessage(msg.chat.id, `Hello! You've typed this message: ${msg.text}`);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
